8b09928cf91bf83c3f1ae0bc9d933313b212cf2f
Ivan Kriukov <ivan.kriukov@strana.com>
2024-10-28 16:22:56 +0300

    qwer

---
+++ components/timer/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, Alert } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators as actions } from "./actions";
+import Button from "./buttons";
+
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+  let seconds = parseInt(time % 60, 10);
+  return `${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerInterval: null,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.isPlaying && this.props.isPlaying) {
+      const timerInterval = setInterval(() => {
+        this.props.addSecond();
+      }, 1000);
+      this.setState({ timerInterval });
+    } else if (prevProps.isPlaying && !this.props.isPlaying) {
+      clearInterval(this.state.timerInterval);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timerInterval);
+  }
+
+  render() {
+    const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer } =
+      this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle={"light-content"} />
+        <View style={styles.upper}>
+          <Text style={styles.time}>
+            {formatTime(timerDuration - elapsedTime)}
+          </Text>
+        </View>
+        <View style={styles.lower}>
+          {!isPlaying && (
+            <Button iconName={"play-circle"} onPress={startTimer} />
+          )}
+          {isPlaying && (
+            <Button iconName={"stop-circle"} onPress={restartTimer} />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lower: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  time: {
+    color: "#ffffff",
+    fontSize: 120,
+    fontWeight: "100",
+  },
+});
+
+function mapStateToProps(state) {
+  const { isPlaying, elapsedTime, timerDuration } = state;
+  return {
+    isPlaying,
+    elapsedTime,
+    timerDuration,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    startTimer: bindActionCreators(actions.startTimer, dispatch),
+    restartTimer: bindActionCreators(actions.restartTimer, dispatch),
+    addSecond: bindActionCreators(actions.addSecond, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
